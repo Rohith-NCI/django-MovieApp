8f3436f3b119d50ad7cfd70475a6a34f13288574
Rohith-A <rohithchintu1914@gmail.com>
2023-10-09 17:01:01 +0100

    deployment changes

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -2,8 +2,8 @@
 
 sudo systemctl daemon-reload
 systemctl restart gunicorn.service
-sudo cp /nginx/nginx.conf /etc/nginx/sites-available/ci-cd-django
-sudo ln -s /etc/nginx/sites-available/ci-cd-django /etc/nginx/sites-enabled
+sudo cp /nginx/nginx.conf /etc/nginx/sites-available/django-movieApp
+sudo ln -s /etc/nginx/sites-available/django-movieApp/etc/nginx/sites-enabled
 sudo nginx -t
 sudo systemctl restart nginx
 sudo ufw delete allow 8000
