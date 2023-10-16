0927f81bb244b5dd7b5e11aadf128cda9d226aac
Rohith-A <rohithchintu1914@gmail.com>
2023-10-16 14:51:46 +0100

    app deployment config

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,14 +1,35 @@
-# #!/usr/bin/bash
+# # #!/usr/bin/bash
 
-# echo "Pull Finished"
-# sudo systemctl daemon-reload
-# sudo systemctl restart nginx
+# # echo "Pull Finished"
+# # sudo systemctl daemon-reload
+# # sudo systemctl restart nginx
+
+# #!/usr/bin/env bash
+
+# # kill any servers that may be running in the background 
+# sudo pkill -f runserver
+
+
+# # kill frontend servers if you are deploying any frontend
+# # sudo pkill -f tailwind
+# # sudo pkill -f node
+
+# cd /home/ubuntu/django-movieApp/
+
+# # activate virtual environment
+# python3 -m venv venv
+# source venv/bin/activate
+
+# install requirements.txt
+# pip install -r /home/ubuntu/django-movieApp/requirements.txt
+
+# # run server
+# screen -d -m python3 manage.py runserver 0:8080
 
 #!/usr/bin/env bash
 
 # kill any servers that may be running in the background 
 sudo pkill -f runserver
-
 
 # kill frontend servers if you are deploying any frontend
 # sudo pkill -f tailwind
