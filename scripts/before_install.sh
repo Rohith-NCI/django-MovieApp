de9c9db74ee48232e46434e6557b150d80abba27
Rohith-A <rohithchintu1914@gmail.com>
2023-10-18 16:08:12 +0100

    install package cmd change

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -59,8 +59,8 @@ sudo rm -rf /home/ubuntu/install
 # sudo ./install auto
 
 # update os & install python3
-sudo apt-get update
-sudo apt-get install -y python3 python3-dev python3-pip python3-venv
+sudo yum update
+sudo yum install -y python3 python3-dev python3-pip python3-venv
 pip install --user --upgrade virtualenv
 
 # delete app
